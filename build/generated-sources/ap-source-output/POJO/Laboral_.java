21b855502f4fe6c536fb5f6e1afcc160c0918ad5
PAVILION <PAVILION@192.168.1.57>
2017-12-03 21:44:54 -0500

    #1tecnicas

--- build/generated-sources/ap-source-output/POJO/Laboral_.java
+++ build/generated-sources/ap-source-output/POJO/Laboral_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-12-03T18:16:21")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-12-03T21:39:26")
 @StaticMetamodel(Laboral.class)
 public class Laboral_ { 
 
